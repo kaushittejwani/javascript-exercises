d51ffc87bd1727e9ff29d8d2598da7b3b5b2b533
kaushit tejwani <tejwanikaushit@gmail.com>
2022-11-24 17:37:06 +0530

    second commit

---
+++ conditonal-statements.js
@@ -0,0 +1,17 @@
+var a =20
+var b=25;
+if(a==b){
+    console.log("both are same value");
+}
+else if(a<b){
+    console.log("a is less than b");
+}
+ else if(a<=b){
+    console.log("a is less than equal to b");
+}
+else if(a>b){
+    console.log(" a is greater than b");
+}
+else{
+    console.log("a is greater than or euals to b");
+}
