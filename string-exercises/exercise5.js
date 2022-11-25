7d408f12edfa67eb66bc56571419fcc7fbe7f20d
kaushit tejwani <tejwanikaushit@gmail.com>
2022-11-25 10:47:19 +0530

    new file addded

--- string-exercises/exercise5.js
+++ string-exercises/exercise5.js
@@ -1,13 +1,11 @@
-//Write a JavaScript function to convert a string in abbreviated form
+//write a programm to capitalize first letter of the word
 
-const prompt = require("prompt-sync")({sigint:true});
-count =0;
-var str=prompt("enter any string");
-var len=str.length;
-aberivatedform(str);
- function aberivatedform(str){
-    for(var i=1;i<len-1;i++){
-        count++
-    }
-    console.log(str[0]+count+str[len-1])
-}
+const prompt=require("prompt-sync")({sigint:true});
+var str =prompt("enter any input");
+var a=[];
+   var a =str.charAt([0]).toUpperCase()+str.slice(1);
+   console.log(a);
+
+
+
+   
