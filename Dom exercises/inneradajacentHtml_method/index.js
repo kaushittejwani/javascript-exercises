dde5c1ab868468cc5938a38906f582be4a7edf06
kaushit tejwani <tejwanikaushit@gmail.com>
2022-11-28 09:54:54 +0530

    ne files added

---
+++ Dom exercises/inneradajacentHtml_method/index.js
@@ -0,0 +1,6 @@
+firstdiv.insertAdjacentHTML("afterbegin","<h1>hy</h1>");
+firstdiv.insertAdjacentHTML("beforebegin","<h1>hy welcome</h1>")
+firstdiv.insertAdjacentHTML("beforeend","<h1>hy welcome all</h1>")
+firstdiv.insertAdjacentHTML("afterend","<h1>bye</h1>")
+firstdiv.remove();
+
