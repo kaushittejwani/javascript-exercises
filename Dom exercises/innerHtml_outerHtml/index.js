0e4203042ca151a6a8f170cfb798ce1eb2ac471f
kaushittejwani <95282960+kaushittejwani@users.noreply.github.com>
2022-11-29 17:22:09 +0530

    Delete Dom exercises directory

--- Dom exercises/innerHtml_outerHtml/index.js
+++
@@ -1,5 +0,0 @@
-//document.getElementsByClassName('container')[0].innerHTML="<h1> hy </h1>"
-//document.getElementsByClassName('container')[0].outerHTML="<div class='container2'><h1>hy this is container2 div element</h1></div>";
-console.log(document.body.firstChild.nodeValue)
-console.log(document.body.firstChild.data)
-console.dir(document.body.textContent)
--- Dom exercises/inneradajacentHtml_method/index.js
+++
@@ -1,6 +0,0 @@
-firstdiv.insertAdjacentHTML("afterbegin","<h1>hy</h1>");
-firstdiv.insertAdjacentHTML("beforebegin","<h1>hy welcome</h1>")
-firstdiv.insertAdjacentHTML("beforeend","<h1>hy welcome all</h1>")
-firstdiv.insertAdjacentHTML("afterend","<h1>bye</h1>")
-firstdiv.remove();
-
