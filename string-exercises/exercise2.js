a81246451d2531b25ccefcb9e74c0efab505581b
kaushittejwani <95282960+kaushittejwani@users.noreply.github.com>
2022-11-29 23:57:13 +0530

    Delete string-exercises directory

--- string-exercises/exercise2.js
+++
@@ -1,9 +0,0 @@
- //Write a JavaScript function to split a string and convert it into an array of words.
-const prompt = require('prompt-sync')({sigint:true});
-
- var str=prompt("Enter the String");
- splitintoarray(str);
- 
-  function splitintoarray(str){
-    console.log(str.split(" "))
-  }
--- string-exercises/exercise3.js
+++
@@ -1,8 +0,0 @@
-//Write a JavaScript function to extract a specified number of characters from a string
-const prompt = require('prompt-sync')({sigint:true});
-
-var str=prompt("Enter any string");
-extractcharacters(str);
-function extractcharacters(str){
-    console.log(str.substring(0,4));
-}
