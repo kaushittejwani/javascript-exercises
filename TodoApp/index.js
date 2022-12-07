1f0fbf352830b1cbb7cd3d10fdef8c1bcb7fd32f
kaushittejwani <95282960+kaushittejwani@users.noreply.github.com>
2022-12-07 16:22:00 +0530

    Update index.js
    all features added in the todo app

--- TodoApp/index.js
+++ TodoApp/index.js
@@ -9,7 +9,7 @@ document.getElementById("title").style.height = "40px";
 
 add.addEventListener("click", function (e) {
   e.preventDefault();
-var notes = document.getElementById("inputfield").value;
+  let notes = document.getElementById("inputfield").value;
 
   let a = document.getElementById("title").value;
   if (notes == "" || a == "") {
@@ -31,51 +31,103 @@ var notes = document.getElementById("inputfield").value;
     taskobj.push(notes);
     localStorage.setItem("notes", JSON.stringify(taskobj));
     showtask();
+    document.getElementById("inputfield").value = "";
+    document.getElementById("title").value = "";
   }
-  
 });
 function showtask() {
-  let webstak = localStorage.getItem("notes");
-  if (!webstak) {
-    taskobj = [];
-  } else {
-    taskobj = JSON.parse(webstak);
-  }
-  let html = "";
-  let data = document.getElementById("divcontainer");
-  data.style.textAlign="center";
-  data.style.border="3px solid black"
-  data.style.background="white"
-  data.style.color="black";
-  data.style.width="300px"
-  data.style.marginLeft="650px"
-  data.style.height="40px"
-  
-  taskobj.forEach((items, index) =>{
-    html += `
-  
-    <tr style="paddingLeft:5px";>
-    <th scope="row" style="marginLeft:30px"; >${index+1}</th>
-         <td>${items}</td>
-        <button type="button" class="text-primary" onClick="edittask(${index})">
-          <i class="fa fa-edit"></i>Edit
-        </button>
-      </td>
-      <td>
-        <button type="button" class="text-danger">
-          <i class="fa fa-trash"></i>delete
-        </button>
-      </td>
-    </tr>
-   
-  `;
-   
-  });
-  data.innerHTML = html;
+ 
+    let webstak = localStorage.getItem("notes");
+    if (!webstak) {
+      taskobj = [];
+    } else {
+      taskobj = JSON.parse(webstak);
+    }
+    let html = "";
+    let data = document.getElementById("divcontainer");
+    data.style.textAlign = "center";
+    //data.style.border="3px solid black"
+    data.style.background = "white";
+    data.style.color = "black";
+    data.style.width = "300px";
+    data.style.marginLeft = "650px";
+    data.style.height = "40px";
+
+    taskobj.forEach((items, index) => {
+      html += `<table id="t">
+    
+      <tr id=table-row>
+      <th scope="row" style="marginLeft:30px"; >${index + 1}</th>
+           <td>${items}</td>
+          <button type="button" class="text-primary" onclick="edittask(${index})">
+            <i class="fa fa-edit"></i>Edit
+          </button>
+        </td>
+        <td>
+          <button type="button" class="text-danger" onclick="Deletetask(${index})">
+            <i class="fa fa-trash"></i>delete
+          </button>
+        </td>
+      </tr>
+      </table>
+     
+    `;
+    });
+    data.innerHTML = html;
+
 }
-function edittask(index){
+function edittask(index) {
+  let saveitem = document.getElementById("saveitem");
+  saveitem = index;
   let webstak = localStorage.getItem("notes");
-  taskobj=JSON.parse(webstak);
-  notes.value=taskobj[index]
-  
+  taskobj = JSON.parse(webstak);
+  let a = (document.getElementById("inputfield").value = taskobj[index]);
+  let button = (document.getElementById("add").style.display = "none");
+  let savebutton = (document.getElementById("save").style.display = "block");
+  save(index)
 }
+function save(index){
+let save = document.getElementById("save");
+save.addEventListener("click",function(){
+  let webstak = localStorage.getItem("notes");
+  taskobj = JSON.parse(webstak);
+  let saveindex = document.getElementById("saveitem");
+  console.log(saveindex);
+  //let a = document.getElementById("inputfield").value;
+  let notes=document.getElementById('inputfield');
+  saveindex.value=index;
+
+  taskobj[saveindex.value] = notes.value
+  localStorage.setItem("notes", JSON.stringify(taskobj));
+
+  document.getElementById("add").style.display = "block";
+  document.getElementById("save").style.display = "none";
+  document.getElementById("inputfield").value = "";
+  showtask();
+  Delete(index)
+  
+});
+}
+function Deletetask(index){
+  let webstak = localStorage.getItem("notes");
+  taskobj = JSON.parse(webstak);
+  taskobj.splice(index,1);
+  localStorage.setItem('notes',JSON.stringify(taskobj))
+  showtask();
+}
+
+let Delete = document.getElementById('deleteall');
+Delete.addEventListener('click',function(){
+
+let webstak = localStorage.getItem("notes");
+taskobj = JSON.parse(webstak);
+if(webstak==0){
+   taskobj=[]
+}
+else{
+    taskobj=JSON.parse(webstak);
+    taskobj=[]
+}
+  localStorage.setItem('notes',JSON.stringify(taskobj))
+  showtask();
+});
